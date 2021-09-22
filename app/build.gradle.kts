50daf79a6f389e7a6ead521e3e1fd1e169badc58
Ronell Salunke <ronell1292k@gmail.com>
2021-09-22 23:59:08 +0530

    chore(deps): upgrade Compose and AGP dependencies
    Signed-off-by: Ronell Salunke <ronell1292k@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,14 +58,14 @@ dependencies {
     implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.runtime:runtime-livedata:${rootProject.extra["compose_version"]}")
-    implementation("androidx.activity:activity-compose:1.3.0")
+    implementation("androidx.activity:activity-compose:1.3.1")
 
     //Lifecycle
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
 
     //Navigation
-    implementation("androidx.navigation:navigation-compose:2.4.0-alpha05")
+    implementation("androidx.navigation:navigation-compose:2.4.0-alpha09")
 
     // Room
     implementation("androidx.room:room-runtime:2.3.0")
